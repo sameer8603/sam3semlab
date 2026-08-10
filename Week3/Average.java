ce1e9a1a42951f86a068c88f1ed0cdbd25ed850e
Sameer Azad <sameerazad000@gmail.com>
2026-08-10 12:37:42 +0530

    Add 1-3week files

---
+++ Week3/Average.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Average {
+   public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 3 numbers: ");
+    double a = sc.nextDouble();
+    double b = sc.nextDouble();
+    double c = sc.nextDouble();
+   System.out.println("Average of 3 numbers is: "+((a+b+c)/3));
+    sc.close();
+        }
+      }
