7d20c70dfe4d06ceaa909f8ee4e8f2d6e88fa0c0
Sameer Azad <sameerazad000@gmail.com>
2026-08-17 11:50:31 +0530

    fix week4 folder

---
+++ Week4/DividebyTwo.java
@@ -0,0 +1,17 @@
+package Week4;
+import java.util.Scanner;
+
+public class DividebyTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double n = sc.nextDouble();
+
+        while (n >= 10) {
+            n = n / 2;
+            System.out.println(n);
+        }
+        sc.close();
+    }
+}
