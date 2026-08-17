abe8ccaeee08751be4ccb362123e60c9cf054118
Sameer Azad <sameerazad000@gmail.com>
2026-08-17 10:01:40 +0530

    Add Week4 files

---
+++ SumSeries.java
@@ -0,0 +1,20 @@
+package Week4;
+import java.util.Scanner;
+
+public class SumSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
